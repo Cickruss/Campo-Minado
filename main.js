fb1ab2cf647c4e6c22ccb95dc990b90c20994637
Cickruss <icaromacedo26@gmail.com>
2022-05-05 11:49:27 -0300

    master

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 var campo = []
 var colunas,linhas, bombs;
 
+function iniciar(params) {
+    document.getElementById('p1').style.display = 'none';
+    document.getElementById('p2').style.display = 'block';
+}
 
 function facil(params) {
+    document.getElementById('p2').style.display = 'none';
+    document.getElementById('p3').style.display = 'block';
     linhas = 10
     colunas = 8
     bombs = 10
 }
 function intermediario(params) {
+    document.getElementById('p2').style.display = 'none';
+    document.getElementById('p3').style.display = 'block';
     linhas = 18
     colunas = 14
 }
 function avancado(params) {
+    document.getElementById('p2').style.display = 'none';
+    document.getElementById('p3').style.display = 'block';
     linhas = 24
     colunas = 20
 }
-
+function popup(params) {
+    document.getElementById('p3').style.display = 'none';
+    document.getElementById('popup').style.display = 'block';
+}
+function jogarnov(params) {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('p2').style.display = 'block';
+}
 function gerarcampo(params) {
     for (let i = 0; i < linhas; i++) {
         campo[i] = []
