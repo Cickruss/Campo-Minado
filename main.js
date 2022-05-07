8851507194fe928b1476c12d083b11c26ddac456
Cickruss <icaromacedo26@gmail.com>
2022-05-06 23:42:46 -0300

    Co-authored-by: Guilherme Yokokura <gtadayukey@users.noreply.github.com>

--- main.js
+++ main.js
@@ -9,21 +9,28 @@ function iniciar(params) {
 function facil(params) {
     document.getElementById('p2').style.display = 'none';
     document.getElementById('p3').style.display = 'block';
+    document.getElementById('dificuldadeX').innerText = 'INICIANTE';
     linhas = 10
     colunas = 8
     bombs = 10
+    gerarcampo1()
+
 }
 function intermediario(params) {
     document.getElementById('p2').style.display = 'none';
     document.getElementById('p3').style.display = 'block';
+    document.getElementById('dificuldadeX').innerText = 'INTERMEDIÁRIO';
     linhas = 18
     colunas = 14
+    gerarcampo2()
 }
 function avancado(params) {
     document.getElementById('p2').style.display = 'none';
     document.getElementById('p3').style.display = 'block';
+    document.getElementById('dificuldadeX').innerText = 'AVANÇADO';
     linhas = 24
     colunas = 20
+    gerarcampo3()
 }
 function popup(params) {
     document.getElementById('p3').style.display = 'none';
@@ -33,12 +40,36 @@ function jogarnov(params) {
     document.getElementById('popup').style.display = 'none';
     document.getElementById('p2').style.display = 'block';
 }
-function gerarcampo(params) {
+
+
+function gerarcampo1(params) {
     for (let i = 0; i < linhas; i++) {
         campo[i] = []
         for (let j = 0; j < colunas; j++) {
             campo[i][j] = 0
-            document.getElementById('campo').innerHTML += '<button id="' + j + "," + i + '" onclick="">' + campo[i][j] + '</button>';
+            document.getElementById('campo').innerHTML += '<button class="quadrado_campo_inicante" id="' + j + "," + i + '" onclick="">' + campo[i][j] + '</button>';
+        }
+        document.getElementById('campo').innerHTML += '<br>';
+    }
+    console.table(campo);
+}
+function gerarcampo2(params) {
+    for (let i = 0; i < linhas; i++) {
+        campo[i] = []
+        for (let j = 0; j < colunas; j++) {
+            campo[i][j] = 0
+            document.getElementById('campo').innerHTML += '<button class="quadrado_campo_intermediario" id="' + j + "," + i + '" onclick="">' + campo[i][j] + '</button>';
+        }
+        document.getElementById('campo').innerHTML += '<br>';
+    }
+    console.table(campo);
+}
+function gerarcampo3(params) {
+    for (let i = 0; i < linhas; i++) {
+        campo[i] = []
+        for (let j = 0; j < colunas; j++) {
+            campo[i][j] = 0
+            document.getElementById('campo').innerHTML += '<button class="quadrado_campo_avancado" id="' + j + "," + i + '" onclick="">' + campo[i][j] + '</button>';
         }
         document.getElementById('campo').innerHTML += '<br>';
     }
@@ -55,10 +86,9 @@ function act_bombs(params) {
             j = Math.floor(Math.random()*colunas)
 
                 if (campo[i][j] == 0) {
-                    campo[i][j] = 1
+                    campo[i][j] = '#'
                     cont_bombs++
                     document.getElementById(j + "," + i).innerHTML = '<button id="' + j + "," + i + '" onclick="">1</button>';
                 }
         }
-        console.table(campo);
-    }
+    }    
